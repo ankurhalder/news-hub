8d25d70672e30900f9c2786df8f001893270f873
Ankur Halder <ankur.halder12345@gmail.com>
2024-08-30 20:03:33 +0530

    chore: Add Spinner, NewsItem, and NavBar components

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,77 @@
+import { Component } from "react";
+
+export class NavBar extends Component {
+  render() {
+    return (
+      <div>
+        <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+          <div className="container-fluid">
+            <a className="navbar-brand" href="/">
+              NewsHub
+            </a>
+            <button
+              className="navbar-toggler"
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#navbarSupportedContent"
+              aria-controls="navbarSupportedContent"
+              aria-expanded="false"
+              aria-label="Toggle navigation"
+            >
+              <span className="navbar-toggler-icon"></span>
+            </button>
+            <div
+              className="collapse navbar-collapse"
+              id="navbarSupportedContent"
+            >
+              <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                <li className="nav-item">
+                  <a className="nav-a className= " aria-current="page" href="/">
+                    Home
+                  </a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-a className=" href="/Business">
+                    Business
+                  </a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-a className=" href="/Entertainment">
+                    Entertainment
+                  </a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-a className=" href="/General">
+                    General
+                  </a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-a className=" href="/Health">
+                    Health
+                  </a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-a className=" href="/Science">
+                    Science
+                  </a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-a className=" href="/Sports">
+                    Sports
+                  </a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-a className=" href="/Technology">
+                    Technology
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </nav>
+      </div>
+    );
+  }
+}
+
+export default NavBar;
