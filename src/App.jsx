53a994cf2a745e594eb73432414ab79cc9ba4db4
Ankur Halder <ankur.halder12345@gmail.com>
2024-08-30 21:30:41 +0530

    chore: Update package dependencies and add react-router-dom for routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,24 @@
 import "./App.css";
 
-import { Component } from "react";
+import { Component, Fragment } from "react";
 import NavBar from "./components/NavBar";
 import News from "./components/News";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 export default class App extends Component {
   render() {
     return (
-      <div>
+      <Fragment>
         <NavBar />
-        <News pageSize={5} country="in" category="General" />
-      </div>
+        <BrowserRouter>
+          <Routes>
+            <Route
+              path="/"
+              element={<News pageSize={5} country="in" category="General" />}
+            ></Route>
+          </Routes>
+        </BrowserRouter>
+      </Fragment>
     );
   }
 }
