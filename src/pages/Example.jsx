842733edb6e75c4513524f6f0930493be3f0b5c0
Ankur Halder <ankur.halder12345@gmail.com>
2024-08-30 21:54:56 +0530

    refactor: Update Example page with scoped variables and improved code structure

--- src/pages/Example.jsx
+++ src/pages/Example.jsx
@@ -1,5 +1,36 @@
-function Example() {
-  return <div>Hello World</div>;
+import React from "react";
+
+function ScopeExample() {
+  // Variables to display the results
+  let resultVar = "";
+  let resultLet = "";
+
+  if (true) {
+    var varNumber = 10;
+    var letNumber = 40;
+    resultVar = `Inside block: varNumber = ${varNumber}`;
+    resultLet = `Inside block: letNumber = ${letNumber}`;
+  }
+
+  // Display results based on scope
+  const outsideVarResult = `Outside block: varNumber = ${varNumber}`;
+  const outsideLetResult =
+    "Outside block: letNumber = Error: Variable not accessible";
+  const outsideLetResult1 = `Outside block: letNumber = ${letNumber}`;
+  let Number1 = 40;
+  let String1 = "70";
+  console.log(Number1 * String1);
+  return (
+    <div>
+      <h1>Scope Example</h1>
+      <p>{resultVar}</p>
+      <p>{resultLet}</p>
+      <p>{outsideVarResult}</p>
+      <p>{outsideLetResult}</p>
+      <p>{outsideLetResult1}</p>
+      <p>{String1 - Number1}</p>
+    </div>
+  );
 }
 
-export default Example;
+export default ScopeExample;
